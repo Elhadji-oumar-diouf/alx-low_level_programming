d34e9dc2eec6a689ed1a827005ce1bdebe86c46b
Elhadji Oumar Diouf <elho.diouf1@uvs.edu.sn>
2022-06-07 02:03:50 +0000

    Print all the letters except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowercase;
+	e='e';
+	q='q';
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		if(lowercase != e && lowercase != q)
+		putchar(lowercase);
+	}
+
+	putchar('\n');
+	return (0);
+}
