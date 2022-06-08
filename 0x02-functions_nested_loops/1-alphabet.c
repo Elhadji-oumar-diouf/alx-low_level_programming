587640b68dc6a0f293ab98e87b015eb82d93381d
Elhadji Oumar Diouf <elho.diouf1@uvs.edu.sn>
2022-06-08 03:49:25 +0000

    a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,14 +7,15 @@
  *function alphabet
  *Return: Always 0 (Success);
  */
- 
- void print_alphabet(void)
- {
- 	char Alpha;
 
- 	for (Alpha = 'a' ; Alpha <= 'z' ; Alpha++){
- 	putchar(Alpha);
+void print_alphabet(void)
+{
+	char Alpha;
+
+	for (Alpha = 'a' ; Alpha <= 'z' ; Alpha++)
+ 	{
+ 		putchar(Alpha);
  	}
 
  	putchar('\n');
- }
+}
