3dd3491fcf7600b40cb35a1f950b6b2c86cb3b7c
Elhadji Oumar Diouf <elho.diouf1@uvs.edu.sn>
2022-06-09 01:06:36 +0000

    this function prints evry minite of the day of jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * jack_bauer - Check 
+ * Description: function prints every minute in one day
+ * Return: Nothing
+ */
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			putchar((H / 10) + '0');
+			putchar((H % 10) + '0');
+			putchar(':');
+			putchar((M / 10) + '0');
+			putchar((M % 10) + '0');
+			putchar('\n');
+		}
+	}
+}
