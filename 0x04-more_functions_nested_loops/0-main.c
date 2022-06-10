1a3ed9f582a94c9c05b7e741692f92f413d9f020
Elhadji Oumar Diouf <elho.diouf1@uvs.edu.sn>
2022-06-10 17:45:24 +0000

    a function that checks for uppercase character

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -1,6 +1,6 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
+#include <ctype.h>
 /**
  * main - check the code.
  *
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-int _putchar(char c);
+int _putchar(int c);
 void print_alphabet(void);
+int _isupper(int c);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
