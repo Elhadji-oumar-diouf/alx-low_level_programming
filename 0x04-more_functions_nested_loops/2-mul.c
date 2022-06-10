6f5062a940180e0427e00681e24e92b1f3a0cf9e
Elhadji Oumar Diouf <elho.diouf1@uvs.edu.sn>
2022-06-10 01:40:08 +0000

    The “Fizz-Buzz test” is an interview question designed to help filter out

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * mul - a function that multiplies two integers.
+ * @a: An input int
+ * @b: An input int
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
