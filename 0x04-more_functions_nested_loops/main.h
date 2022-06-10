bda619422a64e71699e2e869ca35064d8fbe6d57
Elhadji Oumar Diouf <elho.diouf1@uvs.edu.sn>
2022-06-10 17:51:40 +0000

    function that checks for a digit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@ int _isupper(int c);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+int _isdigit(int c);
 int print_sign(int n);
 int _abs(int r);
 int print_last_digit(int r);
