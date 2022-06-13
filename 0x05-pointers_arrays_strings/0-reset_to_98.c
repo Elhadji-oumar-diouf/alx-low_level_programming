fd888c49d024d6f43f1ada9f3d0325068ff449ea
Elhadji Oumar Diouf <elho.diouf1@uvs.edu.sn>
2022-06-13 22:15:03 +0000

    hat takes a pointer to an int as parameter and updates the value it points to

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 
 /**
-*
-*
+* Reset_to_98 : is this name of function takes a pointer to an int as parameter
+* @n: integer pointer
+* Return : Nothing
 */
 void reset_to_98(int *n)
 {
-	*n=98;
+	*n = 98;
 }
